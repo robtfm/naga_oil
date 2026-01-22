5902d402b3d7e70f2211c7aeb6d96e108125e75f
robtfm <50659922+robtfm@users.noreply.github.com>
2026-01-22 00:04:50 +0000

    fix directives

--- src/compose/test.rs
+++ src/compose/test.rs
@@ -1298,7 +1298,6 @@ mod test {
         output_eq!(wgsl, "tests/expected/atomics.txt");
     }
 
-    #[cfg(feature = "override_enable_ray_query")]
     #[test]
     fn test_raycasts() {
         let mut composer =
